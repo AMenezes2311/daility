57e0045cb5ca2565c71337bf784e1149dd6b1da3
Andre <andremenezes231@hotmail.com>
2025-04-07 00:30:04 -0400

    Switch to BrowserRouter for clean URLs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import supabase from './helper/supabaseClient';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -32,7 +32,7 @@ function App() {
   }
 
   return (
-    <Router>
+    <Router basename="/daility">
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
