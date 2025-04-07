273d801408da75bbfb91f17e9d1bdc70af9b6bcd
Andre <andremenezes231@hotmail.com>
2025-04-07 00:27:17 -0400

    Update app title, logo, and fix routing for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import supabase from './helper/supabaseClient';
 import Home from './pages/Home';
 import Login from './pages/Login';
